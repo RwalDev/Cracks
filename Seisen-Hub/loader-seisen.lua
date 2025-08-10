1136d37a0e9bb1462624264fe5db56a201ee975f
RwalDev <decomplierxmx@gmail.com>
2025-08-10 21:58:10 +0530

    Update loader-seisen.lua

--- Seisen-Hub/loader-seisen.lua
+++ Seisen-Hub/loader-seisen.lua
@@ -1,18 +1,52 @@
-warn("Seisen Hub - Cracked By Rwal :3")
+print("Seisen Hub - Cracked By Rwal :3")
 MoonSec_StringsHiddenAttr = true
-local fetchedData = {
-    ["7546582051"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_DungeonHeroes.lua",
-    ["7541395924"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_BuildanIsland.lua",
-    ["4093155512"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_Swordburst3.lua",
-    ["7882829745"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_AnimeEternal.lua",
-    ["5995470825"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_HyperShot.lua"
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Seisen Hub - Cracked",
+    LoadingTitle = "Loading Seisen Hub...",
+    LoadingSubtitle = "Cracked by Rwal",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "SeisenHub",
+        FileName = "Config"
+    },
+    Discord = {
+        Enabled = false,
+    },
+    KeySystem = false
+})
+
+local ScriptsTab = Window:CreateTab("Scripts")
+
+local scripts = {
+    ["Dungeon Heroes"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_DungeonHeroes.lua",
+    ["Build an Island"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_BuildanIsland.lua",
+    ["Swordburst 3"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_Swordburst3.lua",
+    ["Anime Eternal"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_AnimeEternal.lua",
+    ["Hyper Shot"] = "https://raw.githubusercontent.com/RwalDev/Cracks/refs/heads/main/Seisen-Hub/Script_HyperShot.lua"
 }
 
-local placeId = tostring(game.PlaceId)
-local scriptUrl = fetchedData[placeId]
-
-if scriptUrl then
-    loadstring(game:HttpGet(scriptUrl))()
-else
-    warn("The Game Is Not Supported")
+for name, url in pairs(scripts) do
+    ScriptsTab:CreateButton({
+        Name = name,
+        Callback = function()
+            pcall(function()
+                loadstring(game:HttpGet(url))()
+            end)
+            Rayfield:ToggleUI()
+        end
+    })
 end
+
+local CreditsTab = Window:CreateTab("Credits")
+
+CreditsTab:CreateParagraph({
+    Title = "Cracked By",
+    Content = "Rwal :3"
+})
+
+CreditsTab:CreateParagraph({
+    Title = "Original Made By",
+    Content = "Seisen"
+})
